73a75db5b9f47ecb0932de604e9015b67d358773
David <davidrobson2106@hotmail.co.uk>
2020-05-30 09:45:27 +0100

    learning different hooks

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './IngredientList.css';
 
-const IngredientList = props => {
+const IngredientList = React.memo(props => {
   return (
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
         {props.ingredients.map(ig => (
-          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
+          <li key={ig.id} onClick={(id) => props.onRemoveItem(ig.id)}>
             <span>{ig.title}</span>
             <span>{ig.amount}x</span>
           </li>
@@ -16,6 +16,6 @@ const IngredientList = props => {
       </ul>
     </section>
   );
-};
+});
 
 export default IngredientList;
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,91 @@
-import React from 'react';
+import React, { useReducer, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 
-function Ingredients() {
+const ingredientReducer = (currentIngredients, action) => {
+    switch(action.type) {
+        case 'SET':
+            return action.ingredients
+        case 'ADD':
+            return [...currentIngredients, action.ingredient]
+        case 'DELETE':
+            return currentIngredients.filter(ing => ing.id !== action.id)
+        default:
+            throw new Error('should not get here')
+    }
+}
+
+const httpReducer = (httpState, action) => {
+    switch(action.type) {
+        case 'SEND':
+            return {loading: true, error: null};
+        case 'RESPONSE':
+            return {...httpState, loading: false};
+        case 'ERROR':
+            return {loading: false, error: action.error};
+        case 'CLEAR':
+            return {...httpState, error: null};
+        default:
+            throw new Error('Should not be reached')
+    }
+}
+
+const Ingredients = () => {
+
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {
+      loading: false, error: null
+  })
+
+    const filteredIngredientsHandler = useCallback(filteredIngredients => {
+      dispatch({type: 'SET', ingredients: filteredIngredients})
+    }, [])
+
+  const addIngredientHandler = useCallback(ingredient => {
+      dispatchHttp({type: 'SEND'});
+      fetch('https://react-hooks-update-3c881.firebaseio.com/ingredients.json', {
+          method: 'POST',
+          body: JSON.stringify(ingredient),
+          headers: { 'Content-Type': 'application/json' }
+      }).then(response => {
+          dispatchHttp({type: 'RESPONSE'});
+          return response.json();
+      }).then(responseData => {
+          dispatch({type: 'ADD', ingredient: {id: responseData.name, ...ingredient}})
+      })
+  }, []);
+
+  const removeIngredientHandler = useCallback(id => {
+      dispatchHttp({type: 'SEND'});
+      fetch(`https://react-hooks-update-3c881.firebaseio.com/ingredients/${id}.json`, {
+          method: 'DELETE'
+      }).then(response => {
+          dispatchHttp({type: 'RESPONSE'});
+          dispatch({type: 'DELETE', id: id})
+      }).catch(err => {
+          dispatchHttp({type: 'ERROR', error: err.message});
+      })
+  }, []);
+
+  const clearError = () => {
+      dispatchHttp({type: 'CLEAR'});
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+
+      {httpState.error && <ErrorModal onClose={clearError} >{httpState.error}</ErrorModal>}
+
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading}/>
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <IngredientList
+            ingredients={userIngredients}
+            onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
   );
